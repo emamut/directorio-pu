491c6367736aeaaaaed2be0d1be1060e88aad040
Faber Vergara <fabervergara@gmail.com>
2020-09-16 13:21:53 -0500

    get data from gist

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-// https://gist.githubusercontent.com/emamut/86b0b780d790fab63a9138baa313a496/raw/ed25d10c9fd5acb4047820e713101015b2b03c50/directorio-pu.json
+const gistURL =
+  'https://gist.githubusercontent.com/emamut/86b0b780d790fab63a9138baa313a496/raw/ed25d10c9fd5acb4047820e713101015b2b03c50/directorio-pu.json';
 
 function App() {
+  const [responseData, setResponseData] = useState([{}]);
+
+  useEffect(() => {
+    axios.get(gistURL).then((response) => setResponseData(response.data));
+  }, [setResponseData, responseData]);
+
   return (
     <div className="container mx-auto">
       <div className="flex mt-5">
@@ -21,6 +29,36 @@ function App() {
                 <th className="border px-4 py-2">Link a Whatsapp</th>
               </tr>
             </thead>
+            <tbody>
+              {responseData.map((item, key) => (
+                <tr className="border" key={key}>
+                  <td className="text-lg">{item.name}</td>
+                  <td>{item.class}</td>
+                  <td className="text-center">
+                    <a
+                      className="text-blue-400 hover:text-gray-600"
+                      href={'https://us02web.zoom.us/j/' + item.zoom}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      Zoom
+                    </a>
+                  </td>
+                  <td className="text-center">
+                    <a
+                      className="text-blue-400 hover:text-gray-600"
+                      href={
+                        'https://api.whatsapp.com/send?phone=' + item.whatsapp
+                      }
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      Whatsapp
+                    </a>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
           </table>
         </div>
       </div>
